f2da3d0022cbd1d40f1ba1b835ff3ed859879181
Alejandro Martinez <alejandro.mzrz@gmail.com>
2022-02-28 21:42:56 +0100

    tweets lab

---
+++ src/components/Message.js
@@ -0,0 +1,5 @@
+export default function Message(prop){
+    return(
+        <p className="message"> {prop.message} </p>
+    )
+}
---
+++ src/components/Timestamp.js
@@ -0,0 +1,5 @@
+export default function Timestamp(prop){
+    return(
+        <span className="timestamp"> {prop.time} </span>
+    )
+}
